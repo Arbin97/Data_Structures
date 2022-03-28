db6bdd760f34820ffb2226c419a30510ee898b6a
Arbin97 <classic.arbind@gmail.com>
2022-03-28 17:48:47 +0545

    checking signature here

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define vi vector<int>
+#define pp pair<int,int>
+#define vpp vector<pair<int,int> >
+#define se set<int>
+#define pb push_back
+#define mp map<int,int>
+#define maxx INT_MAX
+#define minn INT_MIN
+#define modulo 1e9+7;
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+int32_t main()
+{
+    FAST
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int a; cin>>a;
+    cout<<a; 
+
+
+}
